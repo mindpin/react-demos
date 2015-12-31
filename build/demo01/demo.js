2ba2658ab88fd05f76e1c90743f1e340636a2893
fushang318 <fushang318@gmail.com>
2015-12-31 12:25:46 +0800

    add demo01

--- build/demo01/demo.js
+++ build/demo01/demo.js
@@ -1,1 +1,1 @@
-ReactDOM.render(React.createElement("h1", null, "hello"), document.getElementById());
+ReactDOM.render(React.createElement("h1", null, "我是 jsx 生成的"), document.getElementById('react-demo'));
--- src/demo01/demo.cjsx
+++ src/demo01/demo.cjsx
@@ -1,1 +1,1 @@
-ReactDOM.render <h1>hello</h1>, document.getElementById()
+ReactDOM.render <h1>我是 jsx 生成的</h1>, document.getElementById('react-demo')
